9d29042fb6e1ded0110d67cbcb5ad4cc3b35f45a
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-09-14 10:32:58 +1000

    call to action and footer section complete

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -1,9 +1,35 @@
+import logoImage from "@public/logo.svg";
+import Image from "next/image";
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+  { href: "#", label: "Contact" },
+  { href: "#", label: "Privacy Policy" },
+  { href: "#", label: "Terms & Conditions" },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+  return (
+    <section className="py-16">
+      <footer className="container mx-auto">
+        <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between">
+          <div>
+            <Image src={logoImage} alt="Layers logo" />
+          </div>
+          <div>
+            <nav className="flex gap-6">
+              {footerLinks.map((link) => (
+                <a
+                  key={link.label}
+                  href={link.href}
+                  className="text-sm text-white/50"
+                >
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </footer>
+    </section>
+  );
 }
