260d46e3894c200645d56577b3f0e40e30f101ab
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-09-15 14:18:42 +1000

    hero animations, custom cursor, image transitions

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,27 +1,104 @@
+"use client";
+
 import Button from "../components/Button";
 import designExample1Image from "@public/design-example-1.png";
 import designExample2Image from "@public/design-example-2.png";
+import cursorYouImage from "@public/cursor-you.svg";
 import Image from "next/image";
 import Pointer from "../components/Pointer";
-
-// 48:29
+import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [leftPointerScope, leftPointerAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+
+    leftPointerAnimate([
+      [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [
+        leftPointerScope.current,
+        { x: 0, y: [0, 16, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+
+    rightPointerAnimate([
+      [
+        rightPointerScope.current,
+        { opacity: 1 },
+        { duration: 0.5, delay: 1.5 },
+      ],
+      [rightPointerScope.current, { y: 0, x: 175 }, { duration: 0.5 }],
+      [
+        rightPointerScope.current,
+        { x: 0, y: [0, 20, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+  }, []);
+
   return (
-    <section className="overflow-x-clip py-24">
+    <section
+      className="overflow-x-clip py-24"
+      style={{
+        cursor: `url(${cursorYouImage.src}), auto`,
+      }}
+    >
       <div className="relative container mx-auto px-8">
-        <div className="absolute top-16 -left-32 hidden xl:block">
-          <Image src={designExample1Image} alt="Design example 1" />
-        </div>
-        <div className="absolute -top-16 -right-64 hidden xl:block">
-          <Image src={designExample2Image} alt="Design example 2" />
-        </div>
-        <div className="absolute top-96 left-56 hidden xl:block">
+        {/* Left Design Example */}
+        <motion.div
+          ref={leftDesignScope}
+          initial={{ opacity: 0, y: 100, x: -100 }}
+          drag
+          className="absolute top-16 -left-32 hidden xl:block"
+        >
+          <Image
+            src={designExample1Image}
+            alt="Design example 1"
+            draggable="false"
+          />
+        </motion.div>
+        <motion.div
+          ref={leftPointerScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}
+          className="absolute top-96 left-56 hidden xl:block"
+        >
           <Pointer name="Arthur" />
-        </div>
-        <div className="absolute -top-4 right-80 hidden xl:block">
+        </motion.div>
+        {/* Right Design Example */}
+        <motion.div
+          ref={rightDesignScope}
+          initial={{ opacity: 0, y: 100, x: 100 }}
+          drag
+          className="absolute -top-16 -right-64 hidden xl:block"
+        >
+          <Image
+            src={designExample2Image}
+            alt="Design example 2"
+            draggable="false"
+          />
+        </motion.div>
+        <motion.div
+          ref={rightPointerScope}
+          initial={{ opacity: 0, y: 100, x: 275 }}
+          className="absolute -top-4 right-80 hidden xl:block"
+        >
           <Pointer name="Crystal" color="red" />
-        </div>
+        </motion.div>
         <div className="flex justify-center">
           <div className="inline-flex rounded-full bg-pink-400 bg-gradient-to-r from-purple-400 px-3 py-1 font-semibold text-neutral-950">
             ✨ $7.5M seed round raised
